af78c923b7081daccd144c3351aa4e11db328acf
SerhiiDvorak <serhii.dvorak@customertimes.com>
2023-04-02 11:47:34 +0300

    Add project files.

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using CoinViewer.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinViewer.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DetailsViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public DetailsViewModel DetailsVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            DetailsVM = new DetailsViewModel();
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            DetailsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DetailsVM;
+            });
+        }
+    }
+}
